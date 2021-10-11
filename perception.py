a5d087ac61e287f33e17b4d710e6699de719c4d9
daryl <darylleesy@gmail.com>
2021-10-12 08:21:23 +1100

    Implemented File reading, Downsampling and started DBSCAM

--- perception.py
+++ perception.py
@@ -1,8 +1,40 @@
 import open3d as o3d
-PATH = "./data/0000000000.pcd"
-pcds = [o3d.io.read_point_cloud(PATH)]
-# print how many points there are
-print(pcds)
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+PATH = './data/'
+
+
+def load_points(path, vox_size=0.2):
+    pcds = []
+    file_num = len(os.listdir(path))
+    for i in range(0, file_num):
+        # Read file
+        f = PATH + str(i).zfill(10) + '.pcd'
+        pcd = o3d.io.read_point_cloud(f)
+
+        # Downsampling
+        pcd = pcd.voxel_down_sample(voxel_size=vox_size)
+
+        with o3d.utility.VerbosityContextManager(
+                o3d.utility.VerbosityLevel.Debug) as cm:
+            labels = np.array(
+                pcd.cluster_dbscan(eps=0.5, min_points=6, print_progress=True))
+
+        max_label = labels.max()
+        print(f"point cloud has {max_label + 1} clusters")
+        colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
+        colors[labels < 0] = 0
+        pcd.colors = o3d.utility.Vector3dVector(colors[:, :3])
+
+        pcds.append(pcd)
+
+    
+    return pcds
+
+pcds = load_points(PATH)
+
 
 # Visualise the PCD file
 o3d.visualization.draw_geometries(pcds)
